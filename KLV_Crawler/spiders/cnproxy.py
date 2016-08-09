e4c1708cdc710f924e0181e6bffba0ab05aeda64
jianzhang <vkea95@gmail.com>
2016-08-08 21:41:13 -0700

    增加了判断代理服务器是否可用的代码，但并没有实现多线程

--- KLV_Crawler/spiders/cnproxy.py
+++ KLV_Crawler/spiders/cnproxy.py
@@ -3,24 +3,29 @@ import scrapy
 import logging
 import re
 from KLV_Crawler.items import ProxyItem
+import urllib2, socket
+
+socket.setdefaulttimeout(180)
 
 logger = logging.getLogger()
 
 class CnproxySpider(scrapy.Spider):
     name = "cnproxy"
     allowed_domains = ["cnproxy.com"]
-    start_urls = (
-        # 'http://www.cnproxy.com/',
-        'http://www.cnproxy.com/proxy1.html',
-    )
-
+    indexes    = [1,2,3,4,5,6,7,8,9,10]
+    start_urls = []
+    for i in indexes:
+        url = 'http://www.cnproxy.com/proxy%s.html' % i
+        start_urls.append(url)
+    start_urls.append('http://www.cnproxy.com/proxyedu1.html')
+    start_urls.append('http://www.cnproxy.com/proxyedu2.html')
 
     def parse(self, response):
         # ref: http://www.cnblogs.com/qq78292959/p/3372704.html
         script_div = '//head/script[position()=1]/text()'
         script_re = re.compile('data=(.*)>')
         variables = response.xpath(script_div).extract()[0][1:]
-        logger.debug(variables)
+        # logger.debug(variables)
         list_pair = variables.split(";")
         dict_variable = dict()
 
@@ -29,7 +34,7 @@ class CnproxySpider(scrapy.Spider):
             if len(temp) == 2:
                 dict_variable[temp[0]] = temp[1].replace('"','')
 
-        logger.debug(script_re.findall(response.xpath(script_div).extract()[0]))
+        # logger.debug(script_re.findall(response.xpath(script_div).extract()[0]))
         list_div = '//div[re:test(@id, "proxylisttb")]//tr[position()>1]'
         address_td = '//td[position()=1]/text()'
         protocl_td = '//td[position()=2]/text()'
@@ -47,16 +52,15 @@ class CnproxySpider(scrapy.Spider):
         ports = []
         for port in raw_ports:
             tmp = port.replace('+','')
-            logger.debug('temp:' + tmp)   
             for key  in dict_variable:
 
                 tmp = tmp.replace(key, dict_variable[key])
             ports.append(tmp)
-            logger.debug(tmp)
 
-        logger.debug(variables)
-        logger.debug(protocols[0] +'://'+addresses[0] +':'+ ports[0])
+        # logger.debug(variables)
+        # logger.debug(protocols[0] +'://'+addresses[0] +':'+ ports[0])
         items = []
+        proxyList = list()
         for i in range(len(addresses)):
             item = ProxyItem()
             item['address']  = addresses[i]
@@ -64,7 +68,33 @@ class CnproxySpider(scrapy.Spider):
             item['location'] = locations[i]
             item['port']     = ports[i]
             items.append(item)
-            logger.debug(protocols[i] +'://'+addresses[i] +':'+ ports[i] +'@' + locations[i])
+            # logger.debug(protocols[i] +'://'+addresses[i] +':'+ ports[i] +'@' + locations[i])
+            proxyList.append(addresses[i] +':'+ ports[i])
+        for item in proxyList:
+            # logger.debug(item)
+            if self.is_bad_proxy(item):
+                pass
+                # logger.debug( "Bad Proxy: "+ item)
+            else:
+                logger.debug( item+ " is working")
+            
+
+    def is_bad_proxy(self, pip):    
+        try:        
+            proxy_handler = urllib2.ProxyHandler({'http': pip})        
+            opener = urllib2.build_opener(proxy_handler)
+            opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+            urllib2.install_opener(opener)        
+            req=urllib2.Request('http://www.your-domain.com')  # change the url address here
+            sock=urllib2.urlopen(req)
+        except urllib2.HTTPError, e:        
+            print 'Error code: ', e.code
+            return e.code
+        except Exception, detail:
+
+            print "ERROR:", detail
+            return 1
+        return 0
 
 
 
