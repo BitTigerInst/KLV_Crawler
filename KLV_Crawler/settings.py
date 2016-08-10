845e19035ff3fca2a15e8a7e1e99a67d53ecac54
jianzhang <vkea95@gmail.com>
2016-08-10 12:03:39 -0700

    finish coding

--- KLV_Crawler/settings.py
+++ KLV_Crawler/settings.py
@@ -22,7 +22,7 @@ NEWSPIDER_MODULE = 'KLV_Crawler.spiders'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 100
 REACTOR_THREADPOOL_MAXSIZE = 20
 
 # Configure a delay for requests for the same website (default: 0)
